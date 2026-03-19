869fb82a654775ecc3c020b3f350a71d4e4869f9
Priyanshu <pv254424@gmail.com>
2026-03-20 00:45:51 +0530

    filter

--- src/main/java/filter/AuthenticationFilter.java
+++ src/main/java/filter/AuthenticationFilter.java
@@ -5,7 +5,6 @@ import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
--- src/main/java/filter/LoggingFilter.java
+++ src/main/java/filter/LoggingFilter.java
@@ -1,17 +1,47 @@
 package filter;
 
 import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+@WebFilter("/*")
 public class LoggingFilter implements Filter {
+
+
+
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         Filter.super.init(filterConfig);
     }
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request =  (HttpServletRequest) req;
+
+        long startTime = System.currentTimeMillis();
+
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        String method = ((HttpServletRequest) request).getMethod();
+        String uri = ((HttpServletRequest) request).getRequestURI();
+        String queryString = ((HttpServletRequest) request).getQueryString();
+        String clientIP = request.getRemoteAddr();
+        String userAgent = request.getHeader("User-Agent");
+
+        chain.doFilter(req, resp);
+
+        long duration = System.currentTimeMillis() - startTime;
+
+        HttpServletResponse response = (HttpServletResponse) resp;
+        System.out.printf("[%s] %s %s%s | IP: %s | Status: %d | Time: %dms%n", timestamp, method, uri,
+                queryString != null ? "?" + queryString : "",
+                clientIP, response.getStatus(), duration);
 
     }
 
